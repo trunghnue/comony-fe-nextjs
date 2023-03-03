0f7b54760d9876b3336e382a71dc8c0aab1c19fd
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-03-03 17:19:21 +0700

    add useEffect hook to loading layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./layout.module.scss";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [inLoaded, setInLoaded] = useState(true);
+  const [inLoaded, setInLoaded] = useState(false);
+
+  // cách dùng use effect
+  //Chung: Callback trong use effect luôn được gọi sau khi component mounted
+  //Riêng:
+  // useEffect(callback): Gọi callback mỗi khi re-render & Gọi callback sau khi component thêm element vào DOM
+  // useEffect(callback, []): Chỉ gọi callback 1 lần sau khi component mounted
+  // useEffect(callback, [deps]): Callback sẽ được gọi lại mỗi khi deps thay đổi
+
+  useEffect(() => {
+    setInLoaded(true);
+  }, []);
 
   return (
     <div className={styles.wrapper}>
