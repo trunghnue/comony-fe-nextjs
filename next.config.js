89b998b35701d2a24fff29be52584ea03acded29
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-03-04 11:48:57 +0700

    add variables scss & config global scss

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "./src/asset/scss/style-resources.scss";`,
   },
 };
 
