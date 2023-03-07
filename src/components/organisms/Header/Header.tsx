8f26046d5da5516aa0716acaf6de7158d5159a3e
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-03-07 17:24:29 +0700

    create Header (doing)

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,14 +1,30 @@
 import AppLogo from "@/components/atoms/AppLogo/AppLogo";
 import { useMemo } from "react";
+import styles from "./Header.module.scss";
 
 export default function Header({ bgColor }: { bgColor: string }) {
   const iconColor = useMemo(() => {
     return bgColor === "white" ? "black" : "white";
   }, [bgColor]);
 
+  const bgColorClass = useMemo(() => {
+    return `${bgColor && styles[`-bgColor--${bgColor}`]}`;
+  }, [bgColor]);
+
   return (
-    <header>
+    <header className={`${styles.header} ${bgColorClass}`}>
       <AppLogo iconColor={iconColor} size="small" direction="vertical" />
+      <div className={styles["header_menu"]}>
+        <nav className={styles["header_nav"]}>
+          <div className={styles["header_nav_item"]}>Space Gallery</div>
+          <div className={styles["header_nav_item"]}>For Business</div>
+          <div className={styles["header_nav_item"]}>For Creator</div>
+          <div className={styles["header_nav_item"]}>Contact</div>
+          <div className={styles["header_nav_item"]}>Download App</div>
+          <div className={styles["header_nav_item"]}>Language</div>
+        </nav>
+        <div className="headerButton">Sign in</div>
+      </div>
     </header>
   );
 }
