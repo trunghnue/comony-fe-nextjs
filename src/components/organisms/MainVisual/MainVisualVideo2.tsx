7992abc1876e3d9492b5cbb3cc0411d3238923c4
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-03-20 01:08:26 +0700

    fix MainVisualVideo2.tsx

--- src/components/organisms/MainVisual/MainVisualVideo2.tsx
+++ src/components/organisms/MainVisual/MainVisualVideo2.tsx
@@ -8,28 +8,27 @@ export default function MainVisualVideo2() {
   const { t } = useTranslation("top");
   return (
     <div className={styles.mainVisual}>
-      <div className={styles.mainVisual_background}>
-        <div className={styles.mainVisual_inner}>
-          <div className={styles.mainVisual_heading}>
-            <TextMainVisual id="title1" isVertical type="heading" title={t("mainVisual.title1") || ""} />
-            <TextMainVisual id="title2" isVertical type="heading" title={t("mainVisual.title2") || ""} />
-            <TextMainVisual id="subTitle" isVertical type="subTitle" title={t("mainVisual.subTitle") || ""} />
-          </div>
-          <div className={styles.mainVisual_inner_bannerRight}></div>
-          {/* circleLively */}
-          {/* <CTAButton
-            className={styles.mainVisual_inner_button}
-            type="default"
-            label="Go to the gallery page"
-            labelMb="space gallery"
-            icon
-            iconColor="black"
-            link="/spaces"
-          /> */}
+      <div className={styles.mainVisual_background}></div>
+      <div className={styles.mainVisual_inner}>
+        <div className={styles.mainVisual_heading}>
+          <TextMainVisual id="title1" isVertical type="heading" title={t("mainVisual.title1") || ""} />
+          <TextMainVisual id="title2" isVertical type="heading" title={t("mainVisual.title2") || ""} />
+          <TextMainVisual id="subTitle" isVertical type="subTitle" title={t("mainVisual.subTitle") || ""} />
         </div>
-        <div className={styles.mainVisual_video}>
-          <video controls src={"/video/mainvisual-movie.mp4"} poster="/images/mainvisual-poster.webp" playsInline autoPlay loop muted />
-        </div>
+        <div className={styles.mainVisual_inner_bannerRight}></div>
+        {/* circleLively */}
+        <CTAButton
+          className={styles.mainVisual_inner_button}
+          type="default"
+          label="Go to the gallery page"
+          labelMb="space gallery"
+          icon
+          iconColor="black"
+          link="/spaces"
+        />
+      </div>
+      <div className={styles.mainVisual_video}>
+        <video controls src={"/video/mainvisual-movie.mp4"} poster="/images/mainvisual-poster.webp" playsInline autoPlay loop muted />
       </div>
     </div>
   );
