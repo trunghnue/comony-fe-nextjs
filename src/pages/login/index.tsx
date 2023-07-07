9658c2b189dfa64375e019179a3a4fc47d3c7965
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-07-07 11:33:24 +0700

    udpate Login page: handle click submit, add logic show error

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,20 +3,59 @@ import SectionContainer from "@/components/atoms/SectionContainer/SectionContain
 import DefaultLayout from "@/components/organisms/Layout/DefaultLayout";
 import { GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.scss";
 import { useTranslation } from "next-i18next";
 import LinkText from "@/components/atoms/LinkText/LinkText";
 import LoginForm from "@/components/organisms/LoginForm/LoginForm";
+import { I_LoginRequest } from "@/types/schema/auth";
+import { repositories } from "@/repositories/factories/RepositoryFactory";
 
 const Login = () => {
-  const { t } = useTranslation("login");
+  const { t } = useTranslation(["login", "form"]);
+  const [serverError, setServerError] = useState("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  /*
+   * click login submit button
+   */
+  const handleClickSubmit = async (formValues: I_LoginRequest) => {
+    setIsLoading(true);
+    await repositories
+      .auth()
+      .login({ ...formValues })
+      .then((response: any) => {
+        console.log("🚀 ~ file: index.tsx:19 ~ response:", response);
+      })
+      .catch((error: any) => {
+        console.log("🚀 ~ file: index.tsx:24 ~ error:", error);
+        const errorMessage = error.response?.data?.message;
+
+        if (errorMessage === "UserNotFoundException") {
+          setServerError(t("form.errorMessage.userNotFoundException", { ns: "form" }) || "");
+        }
+
+        if (errorMessage === "PasswordResetRequiredException") {
+          // useLoginUserState.setEmail(formValues.email)
+          // router.push(app.localePath('login-force-pass-reset'))
+        } else if (errorMessage === "UserNotFoundException") {
+          setServerError(t("form.errorMessage.userNotFoundException", { ns: "form" }) || "");
+        } else if (errorMessage === "NotAuthorizedException") {
+          setServerError(t("form.errorMessage.notAuthorizedException", { ns: "form" }) || "");
+        } else if (errorMessage === "Bad Request Exception") {
+          setServerError(t("form.errorMessage.badRequest", { ns: "form" }) || "");
+        } else {
+          setServerError(t("form.errorMessage.normal", { ns: "form" }) || "");
+        }
+      });
+    setIsLoading(false);
+  };
 
   return (
     <DefaultLayout bgColor="gray">
       <SectionContainer bgColor="gray" columns="1" position="left" wrapSize="large">
         <div className={styles.loginTemp}>
-          <Card widthSize="small">
+          <Card widthSize="small" isLoading={isLoading}>
             <Card.Title>
               <h1 className={styles.loginTemp_heading}>{t("login.heading")}</h1>
             </Card.Title>
@@ -25,7 +64,7 @@ const Login = () => {
               <LinkText color="secondary" link="/register" value={t("login.subtext2")} />
             </Card.Subtitle>
             <Card.Body>
-              <LoginForm isLoading={false} serverError="serverError" />
+              <LoginForm isLoading={isLoading} serverError={serverError} emitOnClickSubmit={handleClickSubmit} />
               <div className={styles.loginTemp_toPassChange}>
                 <LinkText link="./pass_reminds-step1" color="secondary" value={t("login.resetting")} />
               </div>
