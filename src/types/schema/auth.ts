e2f1789ef10be663d678ec872784f6371516ac5a
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-07-07 11:28:33 +0700

    update types/schema/auth.ts: export interface I_Login_Response_Data

--- src/types/schema/auth.ts
+++ src/types/schema/auth.ts
@@ -1,3 +1,5 @@
+import { I_Meta } from "./common";
+
 export interface I_LoginRequest {
   email: string;
   password: string;
@@ -7,4 +9,23 @@ export interface I_LoginRequest {
 export interface I_MsgErrorLoginRequest {
   email: string;
   password: string;
+}
+
+export interface I_Login_Response_Data {
+  message: string;
+  $metadata: I_Meta;
+  AuthenticationResult: {
+    AccessToken: string;
+    ExpiresIn: number;
+    IdToken: string;
+    RefreshToken: string;
+    TokenType: string;
+    NewDeviceMetadata: {
+      DeviceKey: string;
+      DeviceGroupKey: string;
+    };
+  };
+  ChallengeName: string;
+  Session: string;
+  ChallengeParameters: object;
 }
