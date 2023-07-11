1c4dae96524a45a93d2f28ee3a5b779f741963c9
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-07-11 17:47:37 +0700

    add I_User_Info_Response

---
+++ src/types/schema/user.ts
@@ -0,0 +1,20 @@
+export interface I_User_Info_Response {
+  email: string;
+  rolesId: number;
+  cognitoUid: string;
+  isActive: number;
+  thumbnailUrl: string;
+  facebookUrl: string;
+  twitterUrl: string;
+  instagramUrl: string;
+  introduction: string;
+  contactUrl: string;
+  companyName: string;
+  companyUrl: string;
+  language: string;
+  lastLoginDate: string;
+  deletedAt: string;
+  createdAt: string;
+  updatedAt: string;
+  id: number;
+}
