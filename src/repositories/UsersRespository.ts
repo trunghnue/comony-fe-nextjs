6dbf5e4f940d709e6220bbb8e692fe68aad95082
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-07-13 17:56:21 +0700

    update UsersRespository

--- src/repositories/UsersRespository.ts
+++ src/repositories/UsersRespository.ts
@@ -2,12 +2,13 @@ import { $axios } from "@/plugins/axios";
 import { I_Response_Type } from "@/types/schema/common";
 import { I_User_Info_Response } from "@/types/schema/user";
 
-const resource = "/user";
+const resource = "/users";
 
 const UserRepository = () => ({
-  userAccount() {
+  userAccount(token: string) {
     return $axios.get<I_Response_Type<I_User_Info_Response>>(`${resource}/account`, {
       headers: {
+        Authorization: `Bearer ${token}`,
         "Cache-Control": "no-cache",
       },
     });
